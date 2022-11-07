d9ff89076081884eab74b714debab2a431051924
Gabriel Valenzuela <gabrielvz955@gmail.com>
2022-11-06 22:14:20 -0300

    Arreglo de objetos, ahora guarda datos ingresados

--- js/main.js
+++ js/main.js
@@ -1,48 +1,35 @@
 
 alert("Bienvenidos a Eduart Bank.\nPor favor complete los campos solicitados.")
 
-let nombre = prompt("Nombre: ");
-let apllido = prompt("Apellido: ");
-let fechaIngresada =("Ingresar Año de nacimiento: ");
-let dniIngr = prompt("Ingresar D.N.I: ");
-let nacionalidad = prompt("Nacionalidad: ")
-let estadoCivil = prompt("Ingrese el numero de opcion depediendo su estado:\n1 - Soltero\n2 - Casado\n3 - Divorciado\n4 - Viudo")
+// Datos de Usuario solicitante
 
-function usuarioNuevo(nombre, apellido, fechaNacmineto, Dni, Nacionalidad){
-    this.nombre = nombre,
-    this.Apellido = apellido,
-    this.feachaNacimineto = fechaIngresada,
-    this.dni = dniIngresado,
-    this.nacionalidad = nacionalidad
+let nombreIngresado = prompt("Nombre: ");
+let apellidoIngresado = prompt("Apellido: ");
+let fechaNacimiento =("Ingresar Año de nacimiento: ");
+let dniIngresado = prompt("Ingresar D.N.I: ");
+let nacionalidadIngresada = prompt("Nacionalidad: ")
+let estadoCivilIngresado = prompt("Ingrese el numero de opcion depediendo su estado:\n1 - Soltero\n2 - Casado\n3 - Divorciado\n4 - Viudo")
+
+// Datos alamacenados
+const usuario = {
+    Nombre: nombreIngresado,
+    Apellido: apellidoIngresado,
+    FechaNacimiento: fechaNacimiento,
+    DNI: dniIngresado,
+    Nacionalidad: nacionalidadIngresada,
+    EstadoCivil: estadoCivilIngresado,
+
 }
 
-console.log(usuarioNuevo.dni);
+function resta(a, b){ 
+    return a + b;
+let fechaActual = 2022;
+}
 
-// let user = prompt("Ingresar nombre de usuario");
 
-// const usuario = {
-//     nombreUsuario: "Gabriel"
-// }
-
-// if (usuario.nombreUsuario == user){
-//     console.log("Usuario aceptado");
-// }else {
-//     console.log("Usuario rechazado");
-// }
-
-// function resta(a, b){ 
-//     return a + b;
-// }
-// let fechaActual = 2022;
-// let nombre = prompt("Ingresar nombre: ");
-
-// console.log("Hola " + nombre)
-// console.log("A continuacion te pediremos que respondas unas preguntas para evaluar tu solitud de prestamos")
-
-// let fechaIngresada = prompt("Ingresar fecha de nacimiento: ")
 
 // let edad = resta(fechaIngresada, fechaActual);
-// // comparando la edad minima con la calculada 
+//  comparando la edad minima con la calculada 
 // if(edad >= 18){ 
 //     console.log("Edad aceptada")
 //     sueldo = prompt("Ingresa ingresos mensuales: ")
@@ -54,9 +41,8 @@ console.log(usuarioNuevo.dni);
 // while(sueldo <= 120.000){ 
 //     alert("Ingresos demaciados bajos.")
 //     sueldo = prompt("Ingrese ganacias mensuales")
-    
+
 // }
 // alert("Ingreso mensuales aceptados!")
 
 // let ocupacion = prompt("Escriba el numero de su profecion o ocupacion:\b 1 - Empleador \b 2 - Relacion de dependencia \b 3 - independinte")
-
