4da2a421d16b5d55a1597363eb131b7077fb994c
Gabriel Valenzuela <gabrielvz955@gmail.com>
2022-11-15 19:18:09 -0300

    Se grego contructor de usuarios

--- js/registro.js
+++ js/registro.js
@@ -4,16 +4,23 @@ const password = document.getElementById("password");
 const passwordVallidation = document.getElementById("repet-password");
 const userEmail = document.getElementById("new-email");
 
-formulario.addEventListener("submit", (e) => {
+formulario.addEventListener("submit", (e) => { // Validacion de formulario.
     e.preventDefault();
-    console.log("funciona");
-    
-    console.log(userName.value);
-    console.log(userEmail.value);
+
+    if(password.value == passwordVallidation.value){ // Validacion de contraseñas.
+        console.log('contraseña Correcta')
+    }else{
+        console.log('contraseñas diferentes');
+        let passwordError = document.getElementById('password-error');
+        passwordError.innerText = "Las contraseñas no coinciden";
+    }
+    // creador de objeto para nuevos usuarios
+    function NewUsers(usuarioNuevo, contraseñaNueva, mailNuevo){
+        this.nombreUsuario = usuarioNuevo;
+        this.contraseñaUsuario = contraseñaNueva;
+        this.mailUsuario = mailNuevo;
+    } 
+    let usuario1 = new NewUsers(userName.value, password.value, userEmail.value);
+    console.log(usuario1);
 });
 
-if(password.value == passwordVallidation.value){
-    alert('contraseña igaules');
-}else{
-    alert('no iguales');
-}
